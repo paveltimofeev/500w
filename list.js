dd06a2db0d7cf35b316e6991c11e5343eb9ba8d7
Pavel Timofeev <paveltimofeev@users.noreply.github.com>
2022-07-07 00:00:55 +0300

    play speech

--- list.js
+++ list.js
@@ -6,12 +6,22 @@ function renderList () {
         div.classList.add('list-item');
         const w = document.createElement('div');
         const t = document.createElement('div');
+        const playButton = document.createElement('div');
+
+        w.classList.add('word');
+        t.classList.add('trans');
+        playButton.classList.add('play');
+        playButton.addEventListener('click', () => { play(item.w); });
         
         w.innerText = `${item.w}`;
         t.innerText = `${item.t}`;
+        playButton.innerHTML = `&#9654;`;
 
-        div.appendChild(w);
-        div.appendChild(t);
+        const wordContainer = document.createElement('div');
+        wordContainer.appendChild(w);
+        wordContainer.appendChild(t);
+        div.appendChild(wordContainer);
+        div.appendChild(playButton);
         container.appendChild(div);
     });
 }
